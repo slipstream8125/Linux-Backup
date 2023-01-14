7d30e6cccc1dc16ac6550c75e32a9eb7e7182987
Slipstream8125 <hatkardhruv@gmail.com>
2023-01-15 00:28:27 +0530

    Update Linux_Backup.sh

--- Linux_Backup.sh
+++ Linux_Backup.sh
@@ -20,7 +20,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
 sudo apt update
 sudo apt install teams -y
-sudo apt install gdebi-core wget -y
+sudo apt install wget -y
 wget -O ~/mailspring.deb "https://updates.getmailspring.com/download?platform=linuxDeb"
 sudo apt install ~/mailspring*.deb
 sudo apt install lutris -y
